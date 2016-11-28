c19054c098a8def445b7c4bdfab0b6b510f52844
Leonardo Fontes <leofontes@gmail.com>
2016-11-28 16:15:19 -0200

    Initiating Database implementation

---
+++ app/src/main/java/me/leofontes/movies/Databases/ContractDB.java
@@ -0,0 +1,67 @@
+package me.leofontes.movies.Databases;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by leo on 28/11/16.
+ */
+
+public class ContractDB {
+    public static final class MovieContract implements BaseColumns {
+        public static final String TABLE_NAME = "movies";
+
+        //Columns
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_SYNOPSIS = "synopsis";
+        public static final String COLUMN_IMAGE = "image";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+
+        //Create String
+        public static final String SQL_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_NAME + " TEXT NOT NULL, " +
+                COLUMN_SYNOPSIS + " TEXT NOT NULL, " +
+                COLUMN_IMAGE + " TEXT NOT NULL, " +
+                COLUMN_RATING + " REAL NOT NULL, " +
+                COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
+                " );";
+    }
+
+    public static final class ReviewContract implements BaseColumns {
+        public static final String TABLE_NAME = "reviews";
+
+        //Columns
+        public static final String COLUMN_AUTHOR = "author";
+        public static final String COLUMN_CONTENT = "content";
+        //Foreign key
+        public static final String COLUMN_MOVIE = "movie_id";
+
+        //Create String
+        public static final String SQL_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_AUTHOR + " TEXT NOT NULL, " +
+                COLUMN_CONTENT + " TEXT NOT NULL, " +
+                COLUMN_MOVIE + " INTEGER, " +
+                " FOREIGN KEY (" + COLUMN_MOVIE + ") REFERENCES " + MovieContract.TABLE_NAME + "(" + MovieContract._ID + ")" +
+                " );";
+    }
+
+    public static final class VideoContract implements BaseColumns {
+        public static final String TABLE_NAME = "videos";
+
+        //Columns
+        public static final String COLUMN_KEY = "key";
+        public static final String COLUMN_NAME = "name";
+        //Foreign key
+        public static final String COLUMN_MOVIE = "movie_id";
+
+        //Create String
+        public static final String SQL_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_KEY + " INTEGER PRIMARY KEY, " +
+                COLUMN_NAME + " TEXT NOT NULL, " +
+                COLUMN_MOVIE + " TEXT NOT NULL, " +
+                " FOREIGN KEY (" + COLUMN_MOVIE + ") REFERENCES " + MovieContract.TABLE_NAME + "(" + MovieContract._ID + ")" +
+                " );";
+    }
+}
---
+++ app/src/main/java/me/leofontes/movies/Databases/MovieDBHelper.java
@@ -0,0 +1,38 @@
+package me.leofontes.movies.Databases;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by leo on 28/11/16.
+ */
+
+public class MovieDBHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "movies.db";
+    public static final int DB_VERSION = 1;
+
+    public MovieDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Create the tables, SQL is specified on the contract classes
+        db.execSQL(ContractDB.MovieContract.SQL_CREATE);
+        db.execSQL(ContractDB.ReviewContract.SQL_CREATE);
+        db.execSQL(ContractDB.VideoContract.SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop the tables, in reverse order because of foreign keys
+        db.execSQL("DROP TABLE IF EXISTS " + ContractDB.VideoContract.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + ContractDB.ReviewContract.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + ContractDB.MovieContract.TABLE_NAME);
+
+        onCreate(db);
+    }
+
+
+}
